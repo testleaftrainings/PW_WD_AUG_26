b26d304040d00f4a289ba476d28cfac6f9d38a56
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2026-08-06 14:48:26 +0530

    Add files via upload

---
+++ Week1Day4/looping.js
@@ -0,0 +1,33 @@
+//for loop
+//when you know the count you can go for for loop
+
+/* switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+} */
+
+        for (let index = 1; index <= 5; index++) {
+            console.log('I will practice ', index, 'time');
+            }
+
+            //while loop when dont know the count
+
+            let i = 1
+
+            while(i<=5){
+                console.log('The value of i is ', i);
+                i++
+                
+            }
+
+            let pageLoaded = false
+
+            while(!pageLoaded){
+                console.log('waiting for page ..');
+                pageLoaded = true
+                
+            }
---
+++ Week1Day4/operators.js
@@ -0,0 +1,116 @@
+/* // Airthmetic operator
+
+//addition
+console.log(12+13); //25
+
+//sub
+console.log(15-5); //10
+
+//multiplication
+console.log(2*3); //6
+
+//division - return the quotient
+console.log(10/2); //5
+
+//modulus - return the reminder
+console.log(10%3); //1
+
+
+//Assigment operator
+
+let a = 10
+
+console.log(a+=5);//a=a+5=15
+
+console.log(a-=10); //a=a-10=5
+
+console.log(a*=2); //a=a*2=10
+
+console.log(a/=4);  //a=a/4=2.5
+ */
+
+//Comparision operators
+
+//== loose equality
+
+/* console.log(2 == '2'); //true
+console.log(2 == true); //false
+console.log(1 == false); //false
+console.log(1 == true); //true
+
+console.log(null==undefined); //true
+
+// === strict equality compare both value and datatype
+
+console.log(2==='2');
+console.log(null === undefined);
+
+//logical operator
+
+//&& (and)
+
+console.log(true && true); //true
+console.log(true && false);  //false
+
+/* if(usernameEntered && passwordEnterted){
+    console.log('proceed to login');
+    
+}else{
+    console.log('Fill all fields');
+    
+} */
+
+// ||(OR)
+
+/* console.log(true || false); //true
+
+// ! (Not) 
+console.log(!true);
+console.log(!false);
+ */
+//unary operator -> works on single operand
+
+//pre increment(++x)
+
+x = 5
+console.log(++x); //6
+//console.log(x);
+
+//post increment(x++)
+
+console.log(x++); //6
+console.log(x); //7
+
+//decrement -> decrement by value by 1
+
+//pre decrement(--y)
+y = 2
+
+console.log(--y); // 1
+
+//post decrement
+console.log(y--);
+console.log(y);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
